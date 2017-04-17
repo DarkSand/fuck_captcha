5bb889e0304ee4829e4fb07bf3b3fe82904d224d
DarkSand <darkwings_love@163.com>
2017-04-17 11:06:53 +0800

    fuck captcha

--- tensorflow_learn/variable_test.py
+++ tensorflow_learn/variable_test.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import tensorflow as tf
-# Create a Variable, that will be initialized to the scalar value 0.
+
+# －创建一个变量, 初始化为标量 0.  初始化定义初值
 state = tf.Variable(0, name="counter")
 
-# Create an Op to add one to `state`.
-
+# 创建一个 op, 其作用是使 state 增加 1
 one = tf.constant(1)
 new_value = tf.add(state, one)
 update = tf.assign(state, new_value)
 
-# Variables must be initialized by running an `init` Op after having
-# launched the graph. We first have to add the `init` Op to the graph.
-init_op = tf.global_variables_initializer()
+# 启动图后, 变量必须先经过`初始化` (init) op 初始化,
+# 才真正通过Tensorflow的initialize_all_variables对这些变量赋初值
+init_op = tf.initialize_all_variables()
 
-# Launch the graph and run the ops.
+# 启动默认图, 运行 op
 with tf.Session() as sess:
-    # Run the 'init' op
+    # 运行 'init' op
     sess.run(init_op)
-    # Print the initial value of 'state'
+
+    # 打印 'state' 的初始值
+    # 取回操作的输出内容, 可以在使用 Session 对象的 run() 调用 执行图时,
+    # 传入一些 tensor, 这些 tensor 会帮助你取回结果.
+    # 此处只取回了单个节点 state，
+    # 也可以在运行一次 op 时一起取回多个 tensor:
+    # result = sess.run([mul, intermed])
     print(sess.run(state))
-    # Run the op that updates 'state' and print 'state'.
+
+    # 运行 op, 更新 'state', 并打印 'state'
     for _ in range(3):
         sess.run(update)
         print(sess.run(state))
 
-# output:
+# 输出:
 
 # 0
 # 1
